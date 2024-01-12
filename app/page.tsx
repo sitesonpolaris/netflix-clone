4f80f72c2c0799d6867db4e5d0169307fa0c975e
Your Name <you@example.com>
2024-01-12 13:20:58 -0500

    updated index

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl text-green-500">Neflix Clone</h1>
-    </main>
-  )
-}

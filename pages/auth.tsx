6f513aa4f639d38178093a1f514dc76019d4d4f5
Your Name <you@example.com>
2024-01-12 06:18:29 -0500

    import react

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import Input from "@/components/Input";
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
